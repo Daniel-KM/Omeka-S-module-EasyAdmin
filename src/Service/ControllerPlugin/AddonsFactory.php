09cea0dd4a1f1cdd062af02f5c8ffe331cedd79c
Daniel Berthereau <Daniel.git@Berthereau.net>
2021-08-23 00:00:00 +0000

    Cleaned code.

--- src/Service/ControllerPlugin/AddonsFactory.php
+++ src/Service/ControllerPlugin/AddonsFactory.php
@@ -7,7 +7,7 @@ use Laminas\ServiceManager\Factory\FactoryInterface;
 
 class AddonsFactory implements FactoryInterface
 {
-    public function __invoke(ContainerInterface $services, $requestedNamed, array $options = null)
+    public function __invoke(ContainerInterface $services, $requestedName, array $options = null)
     {
         return new Addons(
             $services->get('Omeka\HttpClient')
