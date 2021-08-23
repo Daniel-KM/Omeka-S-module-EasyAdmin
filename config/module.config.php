da5989bdddd06fedb11ccf3787c34194f3ff4525
Daniel Berthereau <Daniel.git@Berthereau.net>
2021-08-23 00:00:00 +0000

    Updated languages.

--- config/module.config.php
+++ config/module.config.php
@@ -51,4 +51,14 @@ return [
             ],
         ],
     ],
+    'translator' => [
+        'translation_file_patterns' => [
+            [
+                'type' => 'gettext',
+                'base_dir' => dirname(__DIR__) . '/language',
+                'pattern' => '%s.mo',
+                'text_domain' => null,
+            ],
+        ],
+    ],
 ];
