0a681a43a5955fcb9fe3ca02d8cfa221dc4fcacd
Daniel Berthereau <Daniel.git@Berthereau.net>
2024-03-25 00:00:00 +0000

    Updated Common version.

--- data/scripts/upgrade.php
+++ data/scripts/upgrade.php
@@ -232,10 +232,10 @@ if (version_compare($oldVersion, '3.4.15', '<')) {
 
 
 if (version_compare($oldVersion, '3.4.16', '<')) {
-    if (!method_exists($this, 'checkModuleActiveVersion') || !$this->checkModuleActiveVersion('Common', '3.4.52')) {
+    if (!method_exists($this, 'checkModuleActiveVersion') || !$this->checkModuleActiveVersion('Common', '3.4.54')) {
         $message = new Message(
             $translate('The module %1$s should be upgraded to version %2$s or later.'), // @translate
-            'Common', '3.4.52'
+            'Common', '3.4.54'
         );
         throw new \Omeka\Module\Exception\ModuleCannotInstallException((string) $message);
     }
