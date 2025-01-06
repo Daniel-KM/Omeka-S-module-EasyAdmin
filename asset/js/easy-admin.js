493d104c27e41e9fa3a9f00d32cfac9988205d2d
Daniel Berthereau <Daniel.git@Berthereau.net>
2025-01-06 00:00:00 +0000

    Displayed hidden errors.

--- asset/js/easy-admin.js
+++ asset/js/easy-admin.js
@@ -11,21 +11,30 @@ $(document).ready(function() {
             });
     };
 
+    const showProcessTask = function () {
+        const currentTask = $('input[type=radio].fieldset-process:checked');
+        const currentTaskVal = currentTask.val();
+        // Get the current value and reset and hide all of them.
+        const radioTasks = $('input[type=radio].fieldset-process');
+        radioTasks.prop('checked', false);
+        $('fieldset.field-container > fieldset').hide();
+        // Show the selected container if any.
+        if (currentTaskVal && currentTaskVal !== '') {
+            currentTask.prop('checked', true);
+            $('fieldset.field-container > fieldset.' + currentTaskVal).show();
+        }
+    }
+
     $('.check-and-fix fieldset.field-container > fieldset').hide();
 
-    $('.check-and-fix .fieldset-process').on('click', function() {
-        // Reset all other processes when one is set.
-        $('.fieldset-process').prop('checked', false);
-        $(this).prop('checked', true);
-        // Hide all containers, except the current one.
-        $('fieldset.field-container > fieldset').hide();
-        $('fieldset.field-container > fieldset.' + $(this).val()).show();
-    });
+    $('.check-and-fix .fieldset-process').on('click', showProcessTask);
 
     $('input[name="toggle_tasks_with_warning"]').on('click', hideTasksWarning);
 
     /* Init */
 
     hideTasksWarning();
+
+    showProcessTask();
 
 });
