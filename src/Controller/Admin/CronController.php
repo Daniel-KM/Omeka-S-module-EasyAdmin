b7f5988dc61a3c7dbbd1d91e3f660b350cd0fc69
Daniel Berthereau <Daniel.git@Berthereau.net>
2026-01-05 00:00:00 +0000

    Restructured integrated cron management.

--- src/Controller/Admin/CronController.php
+++ src/Controller/Admin/CronController.php
@@ -10,18 +10,28 @@ class CronController extends AbstractActionController
 {
     public function indexAction()
     {
+        $settings = $this->settings();
+
         /** @var \EasyAdmin\Form\CronForm $form */
         $form = $this->getForm(\EasyAdmin\Form\CronForm::class);
-
-        $cronTasks = $this->settings()->get('easyadmin_cron_tasks') ?: [];
-
         $form->init();
-        $form->setData([
-            'easyadmin_cron_tasks' => $cronTasks,
-        ]);
+
+        // Load current settings.
+        $cronSettings = $settings->get('easyadmin_cron', []);
+
+        // Convert settings to form data.
+        $formData = $form->prepareDataFromSettings($cronSettings);
+        $form->setData($formData);
+
+        // Prepare view data.
+        $lastRun = $settings->get('easyadmin_cron_last');
+        $cronCommand = $this->buildCronCommand();
 
         $view = new ViewModel([
             'form' => $form,
+            'lastRun' => $lastRun,
+            'cronCommand' => $cronCommand,
+            'registeredTasks' => $form->getRegisteredTasks(),
         ]);
 
         $request = $this->getRequest();
@@ -31,28 +41,122 @@ class CronController extends AbstractActionController
 
         $params = $request->getPost();
 
+        // Handle "Run now" action.
+        if (!empty($params['run_now'])) {
+            return $this->runNow();
+        }
+
         $form->setData($params);
         if (!$form->isValid()) {
             $this->messenger()->addErrors($form->getMessages());
             return $view;
         }
 
-        $params = $form->getData();
-        unset($params['csrf']);
+        $data = $form->getData();
+        unset($data['csrf']);
 
-        $tasks = $params['easyadmin_cron_tasks'] ?: [];
-        $this->settings()->set('easyadmin_cron_tasks', $tasks);
+        // Convert form data to settings structure.
+        $newSettings = $form->prepareSettingsFromData($data);
+        $settings->set('easyadmin_cron', $newSettings);
 
-        if ($tasks) {
+        // Keep backward compatibility with old setting name during transition.
+        // TODO Remove in a future version.
+        $oldTasks = [];
+        foreach ($newSettings['tasks'] ?? [] as $taskId => $taskSettings) {
+            if (!empty($taskSettings['enabled'])) {
+                $oldTasks[] = $taskId;
+            }
+        }
+        $settings->set('easyadmin_cron_tasks', $oldTasks);
+
+        $enabledCount = count($oldTasks);
+        if ($enabledCount) {
             $msg = new PsrMessage(
                 '{count} tasks defined to be run regularly.', // @translate
-                ['count' => count($tasks)]
+                ['count' => $enabledCount]
             );
         } else {
-            $msg = new PsrMessage('No task defined to be run regularly.'); // @translate
+            $msg = new PsrMessage(
+                'No task defined to be run regularly.' // @translate
+            );
         }
         $this->messenger()->addSuccess($msg);
 
-        return $view;
+        return $this->redirect()->toRoute('admin/easy-admin/cron', [], true);
+    }
+
+    /**
+     * Run all enabled tasks immediately.
+     */
+    protected function runNow()
+    {
+        $services = $this->getServiceLocator();
+        $settings = $services->get('Omeka\Settings');
+
+        $cronSettings = $settings->get('easyadmin_cron', []);
+        $enabledTasks = [];
+        foreach ($cronSettings['tasks'] ?? [] as $taskId => $taskSettings) {
+            if (!empty($taskSettings['enabled'])) {
+                $enabledTasks[$taskId] = $taskSettings;
+            }
+        }
+
+        if (!count($enabledTasks)) {
+            $this->messenger()->addWarning(new PsrMessage(
+                'No tasks are enabled.' // @translate
+            ));
+            return $this->redirect()->toRoute('admin/easy-admin/cron', [], true);
+        }
+
+        // Dispatch the cron job to run all tasks.
+        $dispatcher = $services->get(\Omeka\Job\Dispatcher::class);
+        $job = $dispatcher->dispatch(\EasyAdmin\Job\CronTasks::class, [
+            'tasks' => $enabledTasks,
+            'manual' => true,
+        ]);
+
+        // Update last run time.
+        $settings->set('easyadmin_cron_last', time());
+
+        $urlPlugin = $this->url();
+        $message = new PsrMessage(
+            'Processing cron tasks in background (job {link_job}#{job_id}{link_end}, {link_log}logs{link_end}).', // @translate
+            [
+                'link_job' => sprintf(
+                    '<a href="%s">',
+                    htmlspecialchars($urlPlugin->fromRoute('admin/id', ['controller' => 'job', 'id' => $job->getId()]))
+                ),
+                'job_id' => $job->getId(),
+                'link_end' => '</a>',
+                'link_log' => class_exists('Log\Module', false)
+                    ? sprintf('<a href="%1$s">', $urlPlugin->fromRoute('admin/default', ['controller' => 'log'], ['query' => ['job_id' => $job->getId()]]))
+                    : sprintf('<a href="%1$s" target="_blank">', $urlPlugin->fromRoute('admin/id', ['controller' => 'job', 'action' => 'log', 'id' => $job->getId()])),
+            ]
+        );
+        $message->setEscapeHtml(false);
+        $this->messenger()->addSuccess($message);
+
+        return $this->redirect()->toRoute('admin/easy-admin/cron', [], true);
+    }
+
+    /**
+     * Build the cron command suggestion.
+     */
+    protected function buildCronCommand(): string
+    {
+        /** @var \Laminas\View\Helper\ServerUrl $serverUrl */
+        $serverUrl = $this->viewHelpers()->get('ServerUrl');
+        $basePath = $this->viewHelpers()->get('BasePath');
+
+        $baseUrl = rtrim($serverUrl(), '/') . $basePath();
+        $scriptPath = realpath(OMEKA_PATH . '/modules/EasyAdmin/data/scripts/task.php');
+
+        // Suggest daily execution by default.
+        return sprintf(
+            '0 0 * * * php %s --task="EasyAdmin\\Job\\CronTasks" --user-id=1 --server-url="%s" --base-path="%s"',
+            $scriptPath,
+            rtrim($serverUrl(), '/'),
+            $basePath()
+        );
     }
 }
--- src/Form/CronForm.php
+++ src/Form/CronForm.php
@@ -5,51 +5,227 @@ namespace EasyAdmin\Form;
 use Common\Form\Element as CommonElement;
 use Laminas\EventManager\Event;
 use Laminas\EventManager\EventManagerAwareTrait;
+use Laminas\Form\Element;
 use Laminas\Form\Form;
 
+/**
+ * Cron configuration form.
+ *
+ * Tasks are registered via event 'easyadmin.cron.tasks'. Each task defines:
+ * - id: unique task identifier
+ * - label: display name
+ * - module: source module name
+ * - job: job class to dispatch (optional)
+ * - callback: callable for quick inline tasks (optional)
+ * - frequencies: supported frequencies ['hourly', 'daily', 'weekly'] (optional)
+ * - default_frequency: default frequency (optional, defaults to 'daily')
+ * - options: sub-options for configurable tasks like session cleanup (optional)
+ *
+ * Settings are stored as:
+ * [
+ *     'tasks' => ['task_id' => ['enabled' => true, 'frequency' => 'hourly', 'option' => 'value'], ...],
+ *     'global_frequency' => 'daily', // Used if individual frequencies not set.
+ * ]
+ */
 class CronForm extends Form
 {
     use EventManagerAwareTrait;
 
+    /**
+     * @var array Registered cron tasks from modules.
+     */
+    protected $registeredTasks = [];
+
     public function init(): void
     {
-        // TODO This is a first form for cron, that will be improved: start, rythm, modules, options, short/long.
+        $this->setAttribute('id', 'form-cron');
+
+        // Collect tasks from modules via event.
+        $this->collectTasks();
+
+        // Build task checkboxes.
+        $taskOptions = $this->buildTaskOptions();
 
         $this
-            ->setAttribute('id', 'form-cron')
             ->add([
-                'name' => 'easyadmin_cron_tasks',
-                // TODO Use a full cron config or a single numeric input.
+                'name' => 'cron_tasks',
                 'type' => CommonElement\OptionalMultiCheckbox::class,
                 'options' => [
-                    'label' => 'Tasks to run once a day', // @translate
-                    'label_attributes' => [
-                        'style' => 'display:inline-block',
-                    ],
+                    'label' => 'Scheduled tasks', // @translate
+                    'value_options' => $taskOptions,
+                ],
+                'attributes' => [
+                    'id' => 'cron_tasks',
+                ],
+            ])
+            ->add([
+                'name' => 'cron_frequency',
+                'type' => CommonElement\OptionalRadio::class,
+                'options' => [
+                    'label' => 'Frequency', // @translate
+                    'info' => 'How often tasks should run. For precise control, use server cron.', // @translate
                     'value_options' => [
-                        'session_1h' => 'Clear sessions older than 1 hour', // @translate
-                        'session_2h' => 'Clear sessions older than 2 hours', // @translate
-                        'session_4h' => 'Clear sessions older than 4 hours', // @translate
-                        'session_12h' => 'Clear sessions older than 12 hours', // @translate
-                        'session_1d' => 'Clear sessions older than 1 day', // @translate
-                        'session_2d' => 'Clear sessions older than 2 days', // @translate
-                        'session_8d' => 'Clear sessions older than 8 days', // @translate
-                        'session_40d' => 'Clear sessions older than 40 days', // @translate
-                        'session_100d' => 'Clear sessions older than 100 days', // @translate
+                        'hourly' => 'Hourly', // @translate
+                        'daily' => 'Daily', // @translate
+                        'weekly' => 'Weekly', // @translate
                     ],
                 ],
                 'attributes' => [
-                    'id' => 'easyadmin_cron_tasks',
+                    'id' => 'cron_frequency',
+                    'value' => 'hourly',
                 ],
             ])
         ;
 
+        // Allow modules to add extra elements.
         $event = new Event('form.add_elements', $this);
         $this->getEventManager()->triggerEvent($event);
 
         $inputFilter = $this->getInputFilter();
-
         $event = new Event('form.add_input_filters', $this, ['inputFilter' => $inputFilter]);
         $this->getEventManager()->triggerEvent($event);
+    }
+
+    /**
+     * Collect cron tasks from all modules via event.
+     *
+     * Modules attach to 'easyadmin.cron.tasks' and add their tasks to the
+     * event params['tasks'] array. Example:
+     *
+     * $sharedEventManager->attach(
+     *     \EasyAdmin\Form\CronForm::class,
+     *     'easyadmin.cron.tasks',
+     *     function ($event) {
+     *         $tasks = $event->getParam('tasks', []);
+     *         $tasks['my_task'] = [
+     *             'label' => 'My task description',
+     *             'module' => 'MyModule',
+     *             'job' => \MyModule\Job\MyJob::class,
+     *             'frequencies' => ['hourly', 'daily'],
+     *             'default_frequency' => 'daily',
+     *         ];
+     *         $event->setParam('tasks', $tasks);
+     *     }
+     * );
+     */
+    protected function collectTasks(): void
+    {
+        // Default EasyAdmin tasks.
+        $defaultTasks = [
+            'session' => [
+                'label' => 'Clear old sessions', // @translate
+                'module' => 'EasyAdmin',
+                'task_type' => 'builtin',
+                'frequencies' => ['hourly', 'daily'],
+                'default_frequency' => 'daily',
+                'options' => [
+                    'session_1h' => 'older than 1 hour', // @translate
+                    'session_2h' => 'older than 2 hours', // @translate
+                    'session_4h' => 'older than 4 hours', // @translate
+                    'session_12h' => 'older than 12 hours', // @translate
+                    'session_1d' => 'older than 1 day', // @translate
+                    'session_2d' => 'older than 2 days', // @translate
+                    'session_8d' => 'older than 8 days', // @translate
+                    'session_30d' => 'older than 30 days', // @translate
+                ],
+                'default_option' => 'session_8d',
+            ],
+        ];
+
+        // Collect tasks from other modules via shared event.
+        $event = new Event('easyadmin.cron.tasks', $this, ['tasks' => $defaultTasks]);
+        $this->getEventManager()->triggerEvent($event);
+
+        $this->registeredTasks = $event->getParam('tasks', $defaultTasks);
+    }
+
+    /**
+     * Build value options for the task checkboxes.
+     */
+    protected function buildTaskOptions(): array
+    {
+        $options = [];
+
+        // TODO Translate labels and option label.
+        foreach ($this->registeredTasks as $taskId => $task) {
+            $module = $task['module'] ?? 'Unknown';
+            $label = $task['label'] ?? $taskId;
+
+            // For tasks with sub-options (like session cleanup).
+            if (!empty($task['options'])) {
+                foreach ($task['options'] as $optionId => $optionLabel) {
+                    $options[$optionId] = sprintf('[%s] %s (%s)', $module, $label, $optionLabel);
+                }
+            } else {
+                $options[$taskId] = sprintf('[%s] %s', $module, $label);
+            }
+        }
+
+        return $options;
+    }
+
+    /**
+     * Get all registered tasks.
+     */
+    public function getRegisteredTasks(): array
+    {
+        return $this->registeredTasks;
+    }
+
+    /**
+     * Convert form data to settings structure.
+     *
+     * Converts flat form data to structured settings.
+     */
+    public function prepareSettingsFromData(array $data): array
+    {
+        $settings = [
+            'tasks' => [],
+            'global_frequency' => $data['cron_frequency'] ?? 'daily',
+        ];
+
+        $enabledTasks = $data['cron_tasks'] ?? [];
+        foreach ($this->registeredTasks as $taskId => $task) {
+            // Handle tasks with sub-options.
+            if (!empty($task['options'])) {
+                foreach ($task['options'] as $optionId => $optionLabel) {
+                    if (in_array($optionId, $enabledTasks)) {
+                        $settings['tasks'][$optionId] = [
+                            'enabled' => true,
+                            'frequency' => $data['cron_frequency'] ?? $task['default_frequency'] ?? 'daily',
+                            'parent_task' => $taskId,
+                        ];
+                    }
+                }
+            } else {
+                if (in_array($taskId, $enabledTasks)) {
+                    $settings['tasks'][$taskId] = [
+                        'enabled' => true,
+                        'frequency' => $data['cron_frequency'] ?? $task['default_frequency'] ?? 'daily',
+                    ];
+                }
+            }
+        }
+
+        return $settings;
+    }
+
+    /**
+     * Convert settings structure to form data.
+     */
+    public function prepareDataFromSettings(array $settings): array
+    {
+        $data = [
+            'cron_tasks' => [],
+            'cron_frequency' => $settings['global_frequency'] ?? 'daily',
+        ];
+
+        foreach ($settings['tasks'] ?? [] as $taskId => $taskSettings) {
+            if (!empty($taskSettings['enabled'])) {
+                $data['cron_tasks'][] = $taskId;
+            }
+        }
+
+        return $data;
     }
 }
